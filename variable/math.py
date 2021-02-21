90c7010a37349eb575b22be2aebd886e5593e41c
MohammadMehdiOmid <MohammadMehdiOmid@gmail.com>
2021-02-21 22:24:19 +0330

    created math function

--- variable/math.py
+++ variable/math.py
@@ -2,8 +2,20 @@
 import math
 
 def math_func( first_num,second_num):
-    sum=math.sum(first_num,second_num)
-    print(sum)
+    pow=math.pow(first_num,second_num)
+    print('pow is :',pow)
+
+    cos=math.cos(first_num)
+    print('cos is: ',cos)
+
+    sin=math.sin(second_num)
+    print('sin is: ',sin)
+
+    tan=math.tan(first_num)
+    print('tan is: ', tan)
+
+    sum=float(cos)+float(sin)+float(tan)
+    print("the sum of {0} and {1} and {2} is {3}".format(sin,cos,tan,sum))
 
 
 
