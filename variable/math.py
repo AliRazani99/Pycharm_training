d6125210a975dad698c0613dd74ad72a087ca248
MohammadMehdiOmid <MohammadMehdiOmid@gmail.com>
2021-02-21 22:07:19 +0330

    math function

---
+++ variable/math.py
@@ -0,0 +1,22 @@
+
+import math
+
+def math_func( first_num,second_num):
+    sum=math.sum(first_num,second_num)
+    print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    first_num=int(input("inter your first number :"))
+    second_num= int(input("inter your second number :"))
+    math_func(first_num,second_num)
