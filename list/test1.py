7eba00763e823102d17811b10d2ad290def0f555
MohammadMehdiOmid <MohammadMehdiOmid@gmail.com>
2021-02-25 00:07:05 +0330

    list with for

--- list/test1.py
+++ list/test1.py
@@ -62,3 +62,8 @@ print("squares : ",squares)
 
 print([(x, y) for x in [1,2,3] for y in [3,1,4] if x == y])
 print([(x, y) for x in [1,2,3] for y in [3,1,4] if x != y])
+
+
+knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+for k, v in knights.items():
+    print(k, v)
