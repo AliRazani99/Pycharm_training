f0e098898ecc046f0e6d1eec97ea9ec32a2c2184
MohammadMehdiOmid <MohammadMehdiOmid@gmail.com>
2021-03-14 12:01:54 +0330

    practice and while

--- loop/loops.py
+++ loop/loops.py
@@ -76,4 +76,11 @@ for x in adj:
 #for loops cannot be empty, but if you for some reason have a for loop with no content, put in the pass statement to avoid getting an error.
 for x in [1,2,3]:
     pass
-
+print("End for.................")
+#while
+count=0
+while count<10:
+    print(count)
+    count+=1
+else:
+    print("Finished while")
