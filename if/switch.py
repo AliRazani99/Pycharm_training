9cfbf44882711187fed985b1cb2c8acf5677cbc7
MohammadMehdiOmid <MohammadMehdiOmid@gmail.com>
2021-03-14 12:42:20 +0330

    better switch:)

--- if/switch.py
+++ if/switch.py
@@ -1,15 +1,31 @@
-#Switchers
+# Switchers
 def func(argument):
-    switchers ={
+    switchers = {
 
-        0 : "Zero",
-        1 : "One",
-        2 : "Two",
-        3 : "Three"
+        0: "Zero",
+        1: "One",
+        2: "Two",
+        3: "Three"
 
     }
-    print(switchers.get(argument,'nothing'))
+    print(switchers.get(argument, 'nothing'))
+
+
+def func2():
+    while (True):
+        string = input("Enter your string :")
+        switchers = {
+            'A': 1,
+            'B': 2,
+            'C': 3,
+            'D': 4
+        }
+        print(switchers.get(string, 'nothing'))
+        if string == 'q':
+            break
+
 
 if __name__ == '__main__':
     func(2)
     func(5)
+    func2()
