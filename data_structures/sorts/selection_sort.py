a765e9ae1f7734affd369dcf1bc2a9ef509413eb
MohammadMehdiOmid <MohammadMehdiOmid@gmail.com>
2021-04-11 00:02:01 +0430

    last update

--- data_structures/sorts/selection_sort.py
+++ data_structures/sorts/selection_sort.py
@@ -1,36 +1,31 @@
-class Search:
+class Sort:
     # just for get 5 number from input
-    def get_data(self):
-        x = 0
-        my_array = []
-        count=int(input("How many numbers do you wanna add?"))
-        while (x < count):
-            number = int(input("please inter your number:"))
-            my_array.append(number)
-            x += 1
-
-        print("my array is :", my_array)
-        return my_array
+    def __init__(self, array):
+        self.array = array
 
     # for sorting data
-    def selection_sort(self, my_array):
+    def selection_sort(self):
 
-        for x in range(len(my_array)):
-            for y in range(x + 1, len(my_array)):
+        for x in range(len(self.array)):
+            for y in range(x + 1, len(self.array)):
                 # finding minimum and swapping
-                if my_array[x] > my_array[y]:
-                    temp = my_array[x]
-                    my_array[x] = my_array[y]
-                    my_array[y] = temp
-
-        return my_array
-
-    # just for print data
-    def print_sorted(self, my_array):
-        print("my sorted array is :", my_array)
+                if self.array[x] > self.array[y]:
+                    temp = self.array[x]
+                    self.array[x] = self.array[y]
+                    self.array[y] = temp
+        print("my sorted array is :", self.array)
 
 
 if __name__ == '__main__':
-    obj = Search()
-    my_array = obj.get_data()
-    obj.print_sorted(obj.selection_sort(my_array))
+    # get data
+    x = 0
+    my_array = []
+    count = int(input("How many numbers do you wanna add?"))
+    while (x < count):
+        number = int(input("please inter your number:"))
+        my_array.append(number)
+        x += 1
+    print("my array is :", my_array)
+
+    obj = Sort(my_array)
+    obj.selection_sort()
