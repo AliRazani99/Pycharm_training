967dde69dfb4c698c3726c3101fb112508083cf7
MohammadMehdiOmid <MohammadMehdiOmid@gmail.com>
2021-04-19 15:23:22 +0430

    Adding days

--- PyQt5/days_in_month.py
+++ PyQt5/days_in_month.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import QDate
+from PyQt5.QtCore import QDate,QDateTime, Qt
 
 
 def count_days():
@@ -7,6 +7,16 @@ def count_days():
     print("Days in Month is:{0}".format(days.daysInMonth()))
     print("Days in year is :{0}".format(days.daysInYear()))
 
+def adding_days():
+    date=QDateTime.currentDateTime()
+
+    print("Date of today is :",date.toString(Qt.ISODate))
+    print("Adding 15days is:{0}".format(date.addDays(15).toString(Qt.ISODate)))
+    print("Subtracting 15days is:{0}".format(date.addDays(-15).toString(Qt.ISODate)))
+    print("Adding 5 years is:{0}".format(date.addYears(5).toString(Qt.ISODate)))
+    print("Adding 5 month is:{0}".format(date.addMonths(5).toString(Qt.ISODate)))
+
 
 if __name__ == '__main__':
     count_days()
+    adding_days()
